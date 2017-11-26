2d82266a3e904a5d366fc8b0982197c71e528a5d
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-11-26 14:12:19 -0300

    extracts livingNeighbors variable

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -1,12 +1,10 @@
 class Cell(var alive: Boolean) {
   def checkNeighbors(neighbors: Cell*) = {
-    if(neighbors.count(_.alive) < 2) {
+    val livingNeighbors = neighbors.count(_.alive)
+    if(livingNeighbors < 2 || livingNeighbors > 3) {
       alive = false
     }
-    if(neighbors.count(_.alive) > 3) {
-      alive = false
-    }
-    if(neighbors.count(_.alive) == 3) {
+    if(livingNeighbors == 3) {
       alive = true
     }
   }
