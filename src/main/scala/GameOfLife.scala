ac52a27a2bd5f7e4972e74649fb4d0e4d877a88b
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-11-26 17:17:56 -0300

    it should calculate next generation

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,7 +1,30 @@
 class GameOfLife(board: Array[Array[Cell]]) {
-  def resurrect(position: (Int, Int)) = {
-    board(position._1)(position._2).resurrect()
+  val NEIGHBORS = Array(
+    (-1, -1),(-1, 0),(-1, 1),
+    ( 0, -1),        ( 0, 1),
+    ( 1, -1),(+1, 0),( 1, 1)
+  )
+
+  def getNeighbors(row: Int, column: Int): Array[Cell] = {
+    NEIGHBORS
+      .map(position => (position._1+row, position._2+column))
+         .filter(position => position._1 > 0 && position._1 < board.length
+           && position._2 > 0 && position._2 < board(position._1).length)
+        .map(position => board(position._1)(position._2))
   }
+
+  def nextGeneration() = {
+    for {
+      row <- board.indices
+      column <- board(row).indices
+    } yield {
+      val cell = board(row)(column)
+      val neighbors = getNeighbors(row,column)
+      cell.checkNeighbors(neighbors:_*)
+    }
+  }
+
+  def resurrect(positions: (Int, Int)*) = positions.foreach(position => board(position._1)(position._2).resurrect())
 
   def getBoard = board
 }
--- src/test/scala/GameOfLifeSpec.scala
+++ src/test/scala/GameOfLifeSpec.scala
@@ -4,19 +4,28 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
   it should "create a 5x5 board" in {
     val game = GameOfLife(5, 5)
 
-    game.getBoard.size shouldBe 5
-    game.getBoard(0).size shouldBe 5
+    game.getBoard.length shouldBe 5
+    game.getBoard(0).length shouldBe 5
     game.getBoard(0)(0).isAlive shouldBe false
   }
 
-  it should "create a 5x5 board with one alive cell" in {
+  it should "create a 5x5 board with some alive cell" in {
     val game = GameOfLife(5, 5)
-    game.resurrect((2,3))
+    game.resurrect((2,3), (1,1), (4,1))
 
-    game.getBoard.size shouldBe 5
-    game.getBoard(0).size shouldBe 5
+    game.getBoard.length shouldBe 5
+    game.getBoard(0).length shouldBe 5
     game.getBoard(0)(0).isAlive shouldBe false
     game.getBoard(2)(3).isAlive shouldBe true
+  }
+
+  it should "calculate next generation" in {
+    val game = GameOfLife(5, 5)
+    game.resurrect((1,1))
+
+    game.nextGeneration()
+
+    game.getBoard(1)(1).isAlive shouldBe false
   }
 
 }
