eab893daeca37fb7c1c11c7395b35129ddebfb8e
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-11-26 14:20:28 -0300

    removes if statements

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -1,12 +1,6 @@
 class Cell(var alive: Boolean) {
   def checkNeighbors(neighbors: Cell*) = {
     val livingNeighbors = neighbors.count(_.alive)
-    if(livingNeighbors < 2 || livingNeighbors > 3) {
-      alive = false
-    }
-    if(livingNeighbors == 3) {
-      alive = true
-    }
+    alive = (alive && livingNeighbors >= 2 && livingNeighbors <= 3) || (!alive && livingNeighbors == 3)
   }
-
 }
