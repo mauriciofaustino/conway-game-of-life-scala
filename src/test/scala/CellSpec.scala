de944dd4aaaff99f7fe75b4c6a5a28e71d5df152
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-11-26 13:43:09 -0300

    Living cell should keep alive if it has 2 living neighbors

--- src/test/scala/CellSpec.scala
+++ src/test/scala/CellSpec.scala
@@ -15,4 +15,20 @@ class CellSpec extends FlatSpec with Matchers {
 
     cell.alive shouldBe false
   }
+
+  "Living cell" should "keep alive if it has 2 living neighbors" in {
+    val cell = new Cell(true)
+    val neighbors: Array[Cell] =  Array(
+      new Cell(true), new Cell(true),
+      new Cell(false),new Cell(false),
+      new Cell(false),new Cell(false),
+      new Cell(false),new Cell(false)
+    )
+
+    cell.checkNeighbors(neighbors:_*)
+
+    cell.alive shouldBe true
+  }
+
+  
 }
