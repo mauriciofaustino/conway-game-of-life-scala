b2316d19eaf5d3575a31e21b19b1dd7232bd60eb
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-11-28 10:12:16 -0300

    refactoring

--- src/test/scala/CellSpec.scala
+++ src/test/scala/CellSpec.scala
@@ -3,74 +3,73 @@ import org.scalatest._
 class CellSpec extends FlatSpec with Matchers {
 
   it should "die with less than 2 living neighbors" in {
-    val cell = new Cell(true)
+    val cell = Cell(true)
     val neighbors: Array[Cell] =  Array(
-      new Cell(true), new Cell(false),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false)
+      Cell(true), Cell(false),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false)
     )
-
 
     cell.willSurvive(neighbors:_*) shouldBe false
   }
 
   it should "keep alive if it has 2 living neighbors" in {
-    val cell = new Cell(true)
+    val cell = Cell(true)
     val neighbors: Array[Cell] =  Array(
-      new Cell(true), new Cell(true),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false)
+      Cell(true), Cell(true),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false)
     )
 
     cell.willSurvive(neighbors:_*) shouldBe true
   }
 
   it should "keep alive if it has 3 living neighbors" in {
-    val cell = new Cell(true)
+    val cell = Cell(true)
     val neighbors: Array[Cell] =  Array(
-      new Cell(true), new Cell(true),
-      new Cell(true), new Cell(false),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false)
+      Cell(true), Cell(true),
+      Cell(true), Cell(false),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false)
     )
 
     cell.willSurvive(neighbors:_*) shouldBe true
   }
 
   it should "die if it has more than 3 living neighbors" in {
-    val cell = new Cell(true)
+    val cell = Cell(true)
     val neighbors: Array[Cell] =  Array(
-      new Cell(true), new Cell(true),
-      new Cell(true), new Cell(true),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false)
+      Cell(true), Cell(true),
+      Cell(true), Cell(true),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false)
     )
 
     cell.willSurvive(neighbors:_*) shouldBe false
   }
 
   it should "resurrect if it has exact 3 living neighbors" in {
-    val cell = new Cell(false)
+    val cell = Cell(false)
     val neighbors: Array[Cell] =  Array(
-      new Cell(true), new Cell(true),
-      new Cell(true), new Cell(false),
-      new Cell(false),new Cell(false),
-      new Cell(false),new Cell(false)
+      Cell(true), Cell(true),
+      Cell(true), Cell(false),
+      Cell(false),Cell(false),
+      Cell(false),Cell(false)
     )
 
     cell.willSurvive(neighbors:_*) shouldBe true
   }
 
   it should "be * in string when it's alive" in {
-    val cell = new Cell(true)
+    val cell = Cell(true)
 
     cell.toString shouldBe "*"
   }
 
   it should "be _ in string when it's died" in {
-    val cell = new Cell(false)
+    val cell = Cell(false)
 
     cell.toString shouldBe "_"
   }
